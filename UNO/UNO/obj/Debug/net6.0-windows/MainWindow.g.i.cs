ad6900b960285c50bff902ed63431b31ffdbf8db
DallasSmith1 <mervyndallas.smith@dcmail.ca>
2023-03-04 14:44:09 -0500

    fixed some bugs

--- UNO/UNO/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ UNO/UNO/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -317,7 +317,7 @@ namespace UNO {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UNO;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UNO;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
